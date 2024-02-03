c36e65735729587b513b612cdd4b76cd2c5a0d41
TechAlchmy <worldsmileswithyou@gmail.com>
2024-02-03 14:10:21 +0000

    2023_2_3_2

--- src/layouts/RewardBar.vue
+++ src/layouts/RewardBar.vue
@@ -352,8 +352,9 @@ onMounted(async () => {
 
 <style lang="scss">
 .m-reward-navigation-drawer {
-  height: 100vh !important;
-  bottom: 0px !important;
+  height: 100%!important;
+  width: 100%!important;
+  top: 0px!important;
   z-index: 2147483645 !important;
   background: $agent_card_bg !important;
   .m-reward-menu{
